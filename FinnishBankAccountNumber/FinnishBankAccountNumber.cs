53396ed93cc7bcb6b3c47307c15e13bc37d2a4dd
vartovi <viljo.varto@gmail.com>
2017-06-15 22:01:40 +0300

    Initial commit

---
+++ FinnishBankAccountNumber/FinnishBankAccountNumber.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnishBankAccountNumber
+{
+    class FinnishBankAccountNumber
+    {
+        public string AccountNumber { get; set; }
+        public string Message { get; set; }
+
+        public FinnishBankAccountNumber(string number)
+        {
+            AccountNumber = number;
+        }
+
+        public string ToMachineForm()
+        {
+            string accNumber = string.Empty;
+            if (AccountNumber.Length < 8 || AccountNumber.Length > 14 || AccountNumber[6] != '-')
+            {
+                return "Invalid account number. Proper account number is 8-14 numbers long and contains dash after first 6 numbers";
+            }
+
+            accNumber = AccountNumber.Remove(6, 1);        
+
+            if (!accNumber.All(Char.IsDigit))
+            {
+                return "Invalid account number. No letters allowed";
+            }
+
+            if (accNumber[0].Equals('4') || accNumber[0].Equals('5'))
+            {
+                while (accNumber.Length < 14)
+                {
+                    accNumber = accNumber.Insert(7, "0");
+                }
+            }
+            else
+            {
+                while (accNumber.Length < 14)
+                {
+                    accNumber = accNumber.Insert(6, "0");
+                }
+            }
+
+            string checksum = HasValidChecksum(accNumber);
+
+            if (checksum == accNumber.Substring(accNumber.Length -1))
+            {
+                Message = "Account number is valid. Checksum " + checksum + " is equal to the last digit.";
+            }
+            else
+            {
+                Message = "Account number is invalid. Checksum " + checksum + " is not equal to the last digit.";
+            }
+            return "Account number " + AccountNumber + " in machine form is: " +  accNumber;
+        }
+
+        public string HasValidChecksum(string numberToCheck)
+        {
+            int total = 0;
+
+            for (int i = 12; i >= 0; i--)
+            {
+                int number = int.Parse(numberToCheck[i].ToString());
+              
+                if (i % 2 == 0)
+                {
+                    if ((number * 2) > 9)
+                    {
+                        total = total + 1 + (number * 2) % 10;
+                    }
+                    else
+                    {
+                        total = total + number * 2;
+                    }
+                }
+                else
+                {
+                    total = total + number;
+                }
+            }
+
+            if (total % 10 == 0)
+            {
+                total = 0;
+            }
+            else
+            {
+                total = 10 - total % 10;
+            }
+            
+            return total.ToString();
+        }
+
+
+        public override string ToString()
+        {
+            return AccountNumber;
+        }
+    }
+}
--- FinnishBankAccountNumber/Program.cs
+++ FinnishBankAccountNumber/Program.cs
@@ -10,6 +10,21 @@ namespace FinnishBankAccountNumber
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Converts traditional/old Finnish bank account number to machine form:");
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Input number to convert");
+                string userInput = Console.ReadLine();
+                Console.WriteLine();
+
+                FinnishBankAccountNumber numberToConvert = new FinnishBankAccountNumber(userInput);
+                
+                Console.WriteLine(numberToConvert.ToMachineForm());
+                Console.WriteLine(numberToConvert.Message);
+                
+            }
+            Console.ReadKey();
         }
     }
 }
